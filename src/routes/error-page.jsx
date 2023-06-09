5b55fe3250fb40ed6cfb945bbb394d8c7bb2b7e5
Denys <tymchenko.denys@gmail.com>
2023-06-10 00:46:36 +0300

    error-page styled

--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -1,15 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <main className='min-h-screen flex flex-col justify-center items-center gap-2 font-bold' id='error-page'>
+      <h1 className='text-5xl '>Oops!</h1>
+      <h2 className='text-3xl'>Something went wrong :(</h2>
+      <p className='text-xl font-light italic '>
+        Error: {error.statusText || error.message}
       </p>
-    </div>
+    </main>
   );
 }
