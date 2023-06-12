9003d864792856366bbb765f689f01e5dd3108db
Denys <tymchenko.denys@gmail.com>
2023-06-12 18:54:13 +0300

    getPopularRepos request now containts right url

--- src/api.js
+++ src/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 export const getPopularRepos = (programmingLanguage) => {
-  const url = programmingLanguage === 'All'
-    ? 'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&type=Repositories'
-    : `https://api.github.com/search/repositories?q=language:${programmingLanguage}&sort=stars&order=desc`
-  return axios.get(url)
+    return axios.get(`https://api.github.com/search/repositories?q=stars:%3E1+language:${programmingLanguage}&sort=stars&order=desc`)
     .then(response => response)
     .catch(error => {
       throw error
